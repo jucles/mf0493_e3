2d4a3d996e859d3717c874bcea94df818ff858b7
jucles <joseantonioucles@gmail.com>
2014-06-03 12:37:07 +0200

    UserManager

---
+++ MF0493_e3/Models/UsuarioManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MF0493_e3.Models
+{
+    public class UsuarioManager
+    {
+        public static usuario getUsuario(string username) 
+        {
+            using (MySQL_Entities db = new MySQL_Entities())
+            {
+                var data = from usr in db.usuarios
+                           where usr.username == username
+                           select usr;
+                return data.First();
+            }
+        }
+    }
+}
