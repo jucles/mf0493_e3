4ae1b2304e4a647bb0352e985b0b7718c5d3c667
jucles <joseantonioucles@gmail.com>
2014-06-03 15:09:41 +0200

    Pruebas unitarias

---
+++ Pruebas/UsuarioManagerTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MF0493_e3.Models;
+
+namespace Pruebas
+{
+    /// <summary>
+    /// Summary description for UsuarioManagerTest
+    /// </summary>
+    [TestClass]
+    public class UsuarioManagerTest
+    {
+
+        [TestMethod]
+        public void getUsuarioTest(string dni)
+        {
+
+            usuario u = UsuarioManager.getUsuario("kkk");
+
+            Assert.IsNull(u);
+
+            u = UsuarioManager.getUsuario("admin");
+
+            Assert.IsNotNull(u);
+            Assert.IsTrue(u.activo);
+            Assert.AreEqual(u.password, "aaa111...");
+
+        }
+
+    }
+}
